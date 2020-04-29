4567ef73eecd31f768521b82a2212ff2fbff3b41
MagmaOverlord <tomas.brejl@gmail.com>
2020-04-29 02:57:25 +0000

    Cookies can now be updated via settings for game look

--- static/index.js
+++ static/index.js
@@ -1,3 +1,10 @@
+if ($.cookie('bgcolor') != undefined) {
+    $("body").css('background-color', $.cookie("bgcolor"));
+    $("#game").css('background-color', $.cookie('gmcolor'));
+    $('#object')[0].src = $.cookie('imsrc');
+    $('body').css('color', $.cookie('fncolor'));
+}
+
 var width = $("#game").width();
 var height = $("#game").height();
 
